455ab5bc7bb590cc4d4e624233fb972e43076984
leoaleluia <aleluiasp17@hotmail.com>
2025-03-25 23:16:21 -0300

    Calendario em c
    Escreva um programa que leia o número equivalente ao mês e imprima a quantidade de
    dias deste mês.

---
+++ Calendariio em.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+	int n1 = 0;
+	printf("Digite o número representante do mÇes: ");
+	scanf("%i", &n1);
+	
+	switch(n1) {
+		case 1: printf("Janeiro 31 dias");
+		break;
+		case 2: printf("Fevereiro 29 dias");
+		break;
+		case 3: printf("Março 31 dias");
+		break;
+		case 4: printf("Abril 30 dias");
+		break;
+		case 5: printf("Maio 31 dias");
+		break;
+		case 6: printf("Junho 30 dias");
+		break;
+		case 7: printf("Julhoe 31 dias");
+		break;
+		case 8: printf("Agosto 31 dias");
+		break;
+		case 9: printf("Setembro 30 dias");
+		break;
+		case 10: printf("Outubro 31 dias");
+		break;
+		case 11: printf("novembro 30 dias");
+		break;
+		case 12: printf("Dezembro 31 dias");
+		break;
+	}
+}

5ec2db651f75db41836e80506b454b1094bb9ec1
leoaleluia <aleluiasp17@hotmail.com>
2025-03-28 08:43:41 -0300

    Create Ano Bisexto.c

---
+++ Atividades estruturas condicionais em c. (IFBA)/Ano Bisexto.c
@@ -0,0 +1,22 @@
+/* Chama-se ano bissexto o ano ao qual é acrescentado um dia extra, ficando ele com 366 
+dias, um dia a mais do que os anos normais de 365 dias, ocorrendo a cada quatro anos. 
+Escreva um programa que verifique se um ano é bissexto. Um ano é bissexto se ele é 
+divisível por 4. Entretanto, se o ano é divisível por 100, ele não é bissexto. Mas, se ele for 
+divisível por 400, ele volta a ser bissexto.  
+a. São bissextos os anos: 1600, 1996, 2000, 2004, 2008, 2012, 2016, 2400, 2800, ... 
+b. Não são bissextos: 1500, 1974, 1982, 1983, 1990, 2018, 2022, 2030, 2038, ... */
+
+#include <stdio.h>
+	
+int main(){
+    int ano = 0;
+    
+    printf("Digite o ano: ");
+    scanf("%d", &ano);
+     
+    if ((ano % 400 == 0  ) || (ano % 4 == 0 && ano % 100 != 0)) {
+        printf("Ano bisexto");
+    }else {
+        printf("O ano não é bisexto");
+    }
+} 

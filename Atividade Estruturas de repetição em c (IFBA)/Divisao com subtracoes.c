cf2b0c9b10bbbd34ec30fd51d0c4182bce8ef102
leoaleluia <aleluiasp17@hotmail.com>
2025-03-29 17:11:30 -0300

    Inserindo atividades no repositório de repetiçoes

---
+++ Atividade Estruturas de repetição em c (IFBA)/Divisao com subtracoes.c
@@ -0,0 +1,21 @@
+/*Escreva um programa que calcule o quociente da divisão de A por B (número inteiros e positivos), ou seja, A / B, através de subtrações sucessivas. Esses dois valores são passados pelo usuário através do teclado. */
+
+#include <stdio.h>
+
+int main(){
+	
+	int n1 = 0; int n2 = 0;  int quo = 0;
+	
+	printf("Digite o dividendo: ");
+	scanf("%i", &n1);
+
+	printf("Digite o divisor: ");
+	scanf("%i", &n2);
+	
+	while (n1 >= n2){
+		n1 -= n2;
+		quo += 1;
+	}
+	
+	printf("o Quociente é %i", quo);
+}
